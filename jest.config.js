fac9a640411396a79db141001515f5f3d62d642b
Marco Ng <marcong0131@gmail.com>
2024-01-09 21:25:49 -0500

    next.config.js

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  reactStrictMode: false,
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'http',
+              hostname: 'localhost',
+              port: '3000',
+              pathname: '**'
+          },
+      ]
+  }
+
+}
+
+module.exports = nextConfig
+
